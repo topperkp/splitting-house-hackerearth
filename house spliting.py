d23da70469b08af45b7c1b25951bfc25a932390c
topperkp <72164673+topperkp@users.noreply.github.com>
2020-10-01 01:10:19 +0530

    Add files via upload

---
+++ house spliting.py
@@ -0,0 +1,24 @@
+n = int(input())
+string = list(map(str,input().split()))
+
+l = len(string)
+
+s = ''
+
+if string[0]==string[l-1]:
+    print('YES')
+    for i in string:
+        print(i)
+        if i is '.' :
+            s= s+'B'
+        else:
+            s=s+i    
+
+
+    print(s)
+
+else:
+    pass        
+        
+      
+        
